1c6377054b850d4fb22cfdbee170bf01173bb3f1
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-15 14:23:34 +0200

    added addTodo func

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import ToDos from './Components/ToDos'
+import ToDos from './Components/ToDos';
+import AddToDo from './Components/AddToDo';
+import UUIDGenerator from 'react-native-uuid-generator';
 
 class App extends Component {
   
@@ -11,10 +13,21 @@ class App extends Component {
     ]
   }
 
+  // filtering through the todos and returning all todos except the one with the id in param
   deleteTodo = (id) => {
     const todos = this.state.todos.filter(todo=>{
       return todo.id !== id;
     })
+    this.setState({
+      todos
+    })
+  }
+
+  // grabbing the new todo from AddToDo component, giving it a new id, adding it to the todos array, then updating the state
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    console.log(todo.id);
     this.setState({
       todos
     })
@@ -26,6 +39,7 @@ class App extends Component {
         <h1 className="center blue-text">ToDo App</h1>
         {/* passing the todos data and the delete func to the ToDos component */}
         <ToDos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddToDo addTodo={this.addTodo} />
       </div>
     );
   }
