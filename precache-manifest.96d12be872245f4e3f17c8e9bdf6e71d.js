beeeaf421f0c2753a5b45c70bb05f14d585a5719
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-15 11:57:47 +0200

    Updates

---
+++ precache-manifest.96d12be872245f4e3f17c8e9bdf6e71d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aaea626e7f171cdd81c8ad3bcd66202",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "d04eec4ad7705dc82e97",
+    "url": "/ToDo-App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e2bef16a42bcfc196bd5",
+    "url": "/ToDo-App/static/js/2.3ed655e0.chunk.js"
+  },
+  {
+    "revision": "d04eec4ad7705dc82e97",
+    "url": "/ToDo-App/static/js/main.ca1060f9.chunk.js"
+  },
+  {
+    "revision": "0ed70d04c92491e368c9",
+    "url": "/ToDo-App/static/js/runtime~main.f2c19e31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDo-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
